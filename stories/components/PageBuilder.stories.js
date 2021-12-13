0f474164401d4407af2262fa8126e7e94e82b25a
Mara <alaneraerts@live.be>
2021-12-13 19:01:31 +0100

    first commit

---
+++ stories/components/PageBuilder.stories.js
@@ -0,0 +1,14 @@
+import PageBuilder from "../components/PageBuilder";
+
+export default {
+  title:"PageBuilder",
+  component:PageBuilder
+};
+
+const Template = (args) => <PageBuilder/>
+
+export const FirstStory = {
+  args: {
+    //👇 The args you need here will depend on your component
+  },
+};
